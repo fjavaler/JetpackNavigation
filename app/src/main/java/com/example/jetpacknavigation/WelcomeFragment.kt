256665123c20ed1c63e4ad5176ad5d556e2a65db
Frederick Javalera <frederickjavalera@gmail.com>
2020-03-28 21:35:18 -0600

    Finished. Errors present from lectures.

--- app/src/main/java/com/example/jetpacknavigation/WelcomeFragment.kt
+++ app/src/main/java/com/example/jetpacknavigation/WelcomeFragment.kt
@@ -11,18 +11,21 @@ import androidx.navigation.Navigation
 /**
  * A simple [Fragment] subclass.
  */
-class WelcomeFragment : Fragment() {
+class WelcomeFragment : Fragment()
+{
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+  override fun onCreateView(
+    inflater: LayoutInflater, container: ViewGroup?,
+    savedInstanceState: Bundle?
+  ): View?
+  {
+    val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
-        view.findViewById<Button>(R.id.registerButton).setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_registrationFragment))
+    view.findViewById<Button>(R.id.registerButton)
+      .setOnClickListener(Navigation.createNavigateOnClickListener(R.id.action_welcomeFragment_to_registrationFragment))
 
-        // Inflate the layout for this fragment
-        return view
-    }
+    // Inflate the layout for this fragment
+    return view
+  }
 
 }
